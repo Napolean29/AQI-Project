c4b25e3f2f0f625b943f60f5bfaa91e96443ab63
Napolean29 <54025067+Napolean29@users.noreply.github.com>
2021-04-10 18:43:45 +0530

    Update Plot_AQI.py
    added a general function to generate AQI data

--- Plot_AQI.py
+++ Plot_AQI.py
@@ -1,6 +1,29 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#one function to generate data for every specific year
+def avg_data(year):
+    temp_i = 0
+    average = []
+    for rows in pd.read_csv('Data/AQI/aqi{}.csv'.format(year),chunksize=24):
+        add_var = 0
+        avg = 0.0
+        data = []
+        df = pd.DataFrame(data=rows)
+        for index,row in df.iterrows():
+            data.append(row['PM2.5'])
+        for i in data:
+            if type(i) is float or type(i) is int:
+                add_var = add_var + i
+            elif type(i) is str:
+                if i!='NoData' and i!='PwrFail' and i!='---' and i!='InVld':
+                    temp = float(i)
+                    add_var = add_var + temp
+        avg = add_var/24
+        temp_i = temp_i + 1
+
+        average.append(avg)
+    return average
 
 def avg_data_2013():
     temp_i=0
